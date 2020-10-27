451f4b4aa9433b467cc9000c9eb2cbc9cb489573
Arias Han <arias.han@hkmci.com>
2020-10-27 15:20:43 +0800

    Updates

---
+++ precache-manifest.5e6bb5df65970886fe4bed596afb93fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fe38800573b8d4ac94ad9d930d98d6",
+    "url": "/BMS2-prototype/index.html"
+  },
+  {
+    "revision": "1408f4f5d9fb84baea2e",
+    "url": "/BMS2-prototype/static/css/2.484e6dcb.chunk.css"
+  },
+  {
+    "revision": "6c572651928f4c6bcbcf",
+    "url": "/BMS2-prototype/static/css/main.6242e9fa.chunk.css"
+  },
+  {
+    "revision": "1408f4f5d9fb84baea2e",
+    "url": "/BMS2-prototype/static/js/2.cfa9f78b.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/BMS2-prototype/static/js/2.cfa9f78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c572651928f4c6bcbcf",
+    "url": "/BMS2-prototype/static/js/main.b28bf9f0.chunk.js"
+  },
+  {
+    "revision": "aca792e1e8b2a7297a8b",
+    "url": "/BMS2-prototype/static/js/runtime-main.4c60d954.js"
+  }
+]);
